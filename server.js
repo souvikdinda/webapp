a0d6324ee352cca9024050e5b6c3872516595de3
Souvik Dinda <souvik.dinda.sd@gmail.com>
2023-02-27 09:55:16 -0500

    commit for demo

--- server.js
+++ server.js
@@ -7,5 +7,5 @@ const PORT = process.env.PORT || 3000;
 // To make the backend application listen to port 8080
 // app.listen takes two input parameters, port and callback function
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port: ${PORT}`);
 });
