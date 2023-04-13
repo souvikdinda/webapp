ac7da5d92f85d020515dc0e388dc6e2d077ad325
Souvik Dinda <souvik.dinda.sd@gmail.com>
2023-04-13 15:45:51 -0400

    Revert changes

--- api/routes/index.js
+++ api/routes/index.js
@@ -4,7 +4,7 @@ import productRouter from './product-router.js';
 
 // All requests should have /users after base URL
 export const userData = (app) => {
-    app.use('/v2/user', usersRouter);
+    app.use('/v1/user', usersRouter);
 }
 
 export const healthCheck = (app) => {
@@ -12,5 +12,5 @@ export const healthCheck = (app) => {
 }
 
 export const productData = (app) => {
-    app.use('/v2/product', productRouter);
+    app.use('/v1/product', productRouter);
 }
